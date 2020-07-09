a83d3beee216a48b80128bcfd20e144762c8c7a3
sebaf96 <sefernandez96@gmail.com>
2020-07-09 14:47:02 -0300

    Agregada constant HERO_DICT que fetchea los heroes y los guarda en un dict

--- fetcher.py
+++ fetcher.py
@@ -2,20 +2,34 @@ import json
 import requests
 
 
+def get_hero_dict() -> dict:
+    response = requests.get('https://api.opendota.com/api/heroes')
+    full_hero_dict = json.loads(response.text)
+    min_hero_dict = {}
+
+    for hero in full_hero_dict:
+        min_hero_dict[hero["id"]] = hero["localized_name"]
+
+    return min_hero_dict
+
+
+HERO_DICT = get_hero_dict()
+
+
 def is_radiant(player_slot: int) -> bool:
     return 0 <= player_slot <= 128
 
 
-
-def fetch(player_id: int) -> str:
+def stats(player_id: int) -> str:
     response = requests.get('https://api.opendota.com/api/players/' + str(player_id) + '/recentMatches')
     recent_matches = json.loads(response.text)
 
-    for i in range(5):
-        radiant = is_radiant(recent_matches[i]["player_slot"])
-        wl = "Gano" if radiant == recent_matches[i]["radiant_win"] else "Perdio"
+    for match in recent_matches[:5]:
+        radiant = is_radiant(match["player_slot"])
+        wl = "Gano" if radiant == match["radiant_win"] else "Perdio"
         print(wl, end="  ")
-        print(str(recent_matches[i]["kills"]) + '/' + str(recent_matches[i]["deaths"]) + '/' + str(recent_matches[i]["assists"]))
+        print(HERO_DICT[match["hero_id"]], end="  ")
+        print(str(match["kills"]) + '/' + str(match["deaths"]) + '/' + str(match["assists"]))
 
 
-fetch(134129467)
+stats(134129467)
