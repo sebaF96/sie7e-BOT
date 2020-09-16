d927bdf700b0bd0ddf9a8105a99ed4cec4769832
sebaf96 <sefernandez96@gmail.com>
2020-09-15 21:43:41 -0300

    Finished Info cog

--- cogs/info.py
+++ cogs/info.py
@@ -1,80 +1,47 @@
 import discord
 from discord.ext import commands
-import random
 
 
 class Information(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["ri", "role"], no_pm=True)
-    @commands.guild_only()
-    async def roleinfo(self, ctx, *, role: discord.Role):
-        """Shows information about a role"""
-        guild = ctx.guild
-
-        since_created = (ctx.message.created_at - role.created_at).days
-        role_created = role.created_at.strftime("%d %b %Y %H:%M")
-        created_on = "{} ({} days ago!)".format(role_created, since_created)
-        members = ''
-        i = 0
-        for user in role.members:
-            members += f'{user.name}, '
-            i += 1
-            if i > 30:
-                break
-
-        if str(role.colour) == "#000000":
-            colour = "default"
-            color = ("#%06x" % random.randint(0, 0xFFFFFF))
-            color = int(colour[1:], 16)
-        else:
-            colour = str(role.colour).upper()
-            color = role.colour
-
-        em = discord.Embed(colour=color)
-        em.set_author(name=role.name)
-        em.add_field(name="Users", value=str(len(role.members)))
-        em.add_field(name="Mentionable", value=role.mentionable)
-        em.add_field(name="Hoist", value=role.hoist)
-        em.add_field(name="Position", value=role.position)
-        em.add_field(name="Managed", value=role.managed)
-        em.add_field(name="Colour", value=colour)
-        em.add_field(name='Creation Date', value=created_on)
-        em.add_field(name='Members', value=members[:-2], inline=False)
-        em.set_footer(text=f'Role ID: {role.id}')
-
-        await ctx.send(embed=em)
-
-
-
     @commands.command(aliases=['server', 'si', 'svi'], no_pm=True)
     @commands.guild_only()
     async def serverinfo(self, ctx):
         """See information about the server."""
-        server = ctx.guild
-        total_users = len(server.members)
-        online = len([m for m in server.members if m.status != discord.Status.offline])
-        text_channels = len([x for x in server.channels if isinstance(x, discord.TextChannel)])
-        voice_channels = len([x for x in server.channels if isinstance(x, discord.VoiceChannel)])
-        categories = len(server.channels) - text_channels - voice_channels
-        passed = (ctx.message.created_at - server.created_at).days
-        created_at = "Creado {}. Hace mas de {} dias!".format(server.created_at.strftime("%d %b %Y %H:%M"), passed)
 
-        data = discord.Embed(description=created_at, colour=ctx.author.color)
-        data.add_field(name="Region", value=str(server.region))
-        data.add_field(name="Usuarios", value="{}/{}".format(online, total_users))
-        data.add_field(name="Canales de texto", value=text_channels)
-        data.add_field(name="Canales de voz", value=voice_channels)
-        data.add_field(name="Categorias", value=categories)
-        data.add_field(name="Roles", value=len(server.roles))
-        data.add_field(name="Owner", value=str(server.owner))
-        data.set_footer(text="Server ID: " + str(server.id))
-        data.set_author(name=server.name, icon_url=None or server.icon_url)
-        data.set_thumbnail(url=None or server.icon_url)
+        # Channels count
+        text_channels = len([x for x in ctx.guild.channels if isinstance(x, discord.TextChannel)])
+        voice_channels = len([x for x in ctx.guild.channels if isinstance(x, discord.VoiceChannel)])
+        categories = len(ctx.guild.channels) - text_channels - voice_channels
 
-        await ctx.send(embed=data)
+        # Members count
+        total_members = len(ctx.guild.members)
+        bots = len([b for b in ctx.guild.members if b.bot])
+        moderators_count = len([m for m in ctx.guild.members if m.top_role.name == '@moderator'])
+        roles = len(ctx.guild.roles)
 
+        # Member statuses
+        online = len([m for m in ctx.guild.members if m.status == discord.Status.online])
+        idle = len([m for m in ctx.guild.members if m.status == discord.Status.idle])
+        do_not_disturb = len([m for m in ctx.guild.members if m.status == discord.Status.do_not_disturb])
+        offline = len([m for m in ctx.guild.members if m.status == discord.Status.offline])
+
+        server_info = f"- Created {ctx.guild.created_at.strftime('%d %b %Y')}\n- Region: {ctx.guild.region}"
+        channels_count = f"- Text channels: {text_channels}\n- Voice channels: {voice_channels}\n- Categories: {categories}"
+        member_counts = f"- Members: {total_members}\n- Bots: {bots}\n- Moderators: {moderators_count}\n- Roles: {roles}"
+        member_statuses = f""":green_circle: {online} :yellow_circle: {idle} :red_circle: {do_not_disturb} :white_circle: {offline}"""
+
+        embed = discord.Embed(colour=discord.Colour.orange())
+        embed.add_field(name='Server information', value=server_info, inline=False)
+        embed.add_field(name="Channels count", value=channels_count, inline=False)
+        embed.add_field(name="Members count", value=member_counts, inline=False)
+        embed.add_field(name="Members statuses", value=member_statuses, inline=False)
+
+        embed.set_thumbnail(url=None or ctx.guild.icon_url)
+
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=['ui'], no_pm=True)
     @commands.guild_only()
@@ -82,7 +49,6 @@ class Information(commands.Cog):
         """Get information about a member of a server"""
         server = ctx.guild
         user = member or ctx.message.author
-        avi = user.avatar_url
 
         if user.desktop_status != discord.Status.offline:
             status = 'Connected from Desktop'
@@ -94,28 +60,14 @@ class Information(commands.Cog):
             status = 'Disconnected'
 
         member_number = sorted(server.members, key=lambda m: m.joined_at).index(user) + 1
+        userinfo_value = f"- Account created {user.created_at.__format__('%d %b %Y')}\n- Profile: {user.mention}\n- ID: {user.id}"""
+        memberinfo_value = f"- Member since {user.joined_at.__format__('%d %b %Y')}\n- Member n° {member_number}\n- Top role: {user.top_role.mention if user.top_role.name != '@everyone' else 'None'}"
 
         em = discord.Embed(colour=user.colour)
-
-        userinfo_value = f"- Account created {user.created_at.__format__('%A, %d. %B %Y')}\n" \
-                         f"- Profile: {user.mention}\n" \
-                         f"- ID: {user.id}"
-
-        em.add_field(name='\nUser info', value=userinfo_value, inline=False)
-
-        memberinfo_value = f"- Member since {user.joined_at.__format__('%A, %d. %B %Y')}\n" \
-                           f"- Member n° {member_number}\n" \
-                           f"- Top role: {user.top_role.mention if user.top_role.name != '@everyone' else 'None'}"
-
-        em.add_field(name='\nMember info', value=memberinfo_value, inline=False)
-
-
-        em.add_field(name='\nStatus', value=status)
-
-
-        em.set_thumbnail(url=avi)
+        em.set_thumbnail(url=user.avatar_url)
         em.set_author(name=user, icon_url=server.icon_url)
+        em.add_field(name='User info', value=userinfo_value, inline=False)
+        em.add_field(name='Member info', value=memberinfo_value, inline=False)
+        em.add_field(name='Status', value=status)
 
         await ctx.send(embed=em)
-
-
